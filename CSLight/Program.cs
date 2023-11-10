0f8836f79ffc7624a6107266dd6c8a4590ad09f4
Svisha <kafraro@gmail.com>
2023-11-11 00:00:21 +0300

    Добавьте файлы проекта.

---
+++ CSLight/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float result;
+
+        int x, y;
+
+        x = 5; y = 2;
+        result = Convert.ToSingle(x) / y;
+        Console.WriteLine(result);
+        Console.ReadLine();
+
+        int timeINminute = 130;
+        int hour;
+        int minute;
+
+        hour = timeINminute / 60;
+        minute = timeINminute % 60;
+        Console.WriteLine("hour: " + hour);
+        Console.WriteLine("minute: " + minute);
+
+        int age = 18;
+        age = age + 1;
+        age++;
+        age += 1;
+        age += 1; age += 1;
+
+        int result2 = 5 + (12 - 5) * 3 - 1 + 5 * 2;
+        Console.WriteLine(result2);
+
+
+    }
+}
